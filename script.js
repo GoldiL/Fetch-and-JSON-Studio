b0f8d7dba0a8dd1fd628fb04719c7957d856d184
GoldiL <goldialbina@gmail.com>
2023-06-28 20:07:19 -0500

    First commit

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // TODO: add code here
+
+window.addEventListener("load",function(){
+    this.fetch("https://handlers.education.launchcode.org/static/astronauts.astronauts").then(function(response){
+        response.json().then(function(astronauts){
+            console.log(astronauts)
+            const id=document.getElementById("container");
+            for(let i=0;i<astronauts.length;i++){
+            id.innerHTML +=`
+            <div class="astronaut">
+                <div class="bio">
+                <h3>${astronauts[i].firstName} ${astronauts[i].lastName}</h3>
+             <ul>
+                <li>Hours in space: ${astronauts[i].hoursInSpace}</li>
+                <li>Active: ${astronauts[i].active}</li>
+                <li>Skills: ${astronauts[i].skills.join(",")}</li>
+            </ul>
+            </div>
+            <img class="avatar" src="images/mae-jemison.jpg">
+            </div>`
+            }
+        })
+    })
+})
